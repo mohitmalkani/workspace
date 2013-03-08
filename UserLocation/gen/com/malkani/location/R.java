98b20a545eaa28887f0623f54a2b93f85e34f406
Mohit <mohit@getjar.com>
2013-03-08 11:44:46 -0800

    UserLocation apk written

--- UserLocation/gen/com/malkani/location/R.java
+++ UserLocation/gen/com/malkani/location/R.java
@@ -15,10 +15,13 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int address_text=0x7f070002;
-        public static final int get_address_button=0x7f070001;
-        public static final int location_text=0x7f070000;
-        public static final int menu_settings=0x7f070003;
+        public static final int TextView01=0x7f070001;
+        public static final int TextView02=0x7f070002;
+        public static final int TextView03=0x7f070004;
+        public static final int TextView04=0x7f070005;
+        public static final int linearLayout1=0x7f070000;
+        public static final int linearLayout2=0x7f070003;
+        public static final int menu_settings=0x7f070006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
