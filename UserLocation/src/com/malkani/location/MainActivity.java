9d75b33e7b7b72024bfea1b6b369a175b14b2d9b
Mohit <mohit@getjar.com>
2013-03-07 17:25:40 -0800

    Changes to var name in main activity for lcation app

--- UserLocation/src/com/malkani/location/MainActivity.java
+++ UserLocation/src/com/malkani/location/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	private Location _currentLocation;
+	private Location _currentLocationCHANGE;
 	private LocationManager _locationManager;
 	private TextView _locationText;
 	private TextView _addressText;
